cbe224eefc31311fd06cb5d14c2c7314649414a2
seungwoo <sugohaseyo93@gmail.com>
2019-05-16 15:53:38 +0900

    웹크롤링 완성

--- search/views.py
+++ search/views.py
@@ -1,16 +1,26 @@
+import django
+
+django.setup()
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
 
-from selenium import webdriver
 from bs4 import BeautifulSoup
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
 import time
 
 from log.models import Log
 from search.forms import SearchForm
+import requests
+
+from multiprocessing import Pool, Process
+
+
+def search_urls(url):  # class안에 넣으면 시리얼 에러남
+    req = requests.get(url)
+    html = req.text
+    return html
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -20,10 +30,12 @@ class MainList(View):
         super().__init__(**kwargs)
         self.company_name: str = ''
         self.keywords: str = ''
-        self.naver_shopping_url: str = 'https://shopping.naver.com/'
-        self.driver:webdriver = webdriver.Chrome('chromedriver')
+        self.naver_shopping_url: str = ''
+        self.urls: list = []
         self.result: list = []
-
+        self.pool: Pool = Pool(processes=4)
+        self.search_urls = search_urls
+        self.htmls: list = []
 
     def get(self, request):
         form = SearchForm()
@@ -35,25 +47,40 @@ class MainList(View):
             form.save()
             self.company_name: str = form.cleaned_data['company_name']
             self.keywords: str = form.cleaned_data['keywords']
-            self.search_keywords()
+            self.get_links()
 
         form = SearchForm()
         return render(request, 'index.html', {'form': form})
 
-    def search_keywords(self) -> dict:
-        self.driver.get(self.naver_shopping_url)
-        self.driver.find_element_by_name('query').send_keys(self.keywords)
-        self.driver.find_element_by_xpath('//*[@id="autocompleteWrapper"]/a[2]').click()
-        html:str = self.driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        product_info: BeautifulSoup = soup.select('div.info')
-        seller_info: BeautifulSoup = soup.select('div.info_mall a.mall_img')
+    def get_links(self):
+        for i in range(1, 11):
+            self.naver_shopping_url = f'https://search.shopping.naver.com/search/all.nhn?origQuery={self.keywords}&pagingIndex={i}' \
+                f'&pagingSize=40&viewType=list&sort=rel&frm=NVSHPAG&query={self.keywords}'
+            self.urls.append(self.naver_shopping_url)
+        self.htmls: list = self.pool.map(self.search_urls, self.urls)
+        self.check_items()
+        # self.pool.map(self.check_items, htmls)
 
-        for i, seller in enumerate(seller_info):
-            print(self.company_name, seller_info[i].text)
-            if self.company_name in seller_info[i].text:
-                print('있다')
-            else:
-                print('없다')
+    def check_items(self):
 
-        self.driver.quit()
+        for i, html in enumerate(self.htmls):
+            soup = BeautifulSoup(html, 'html.parser')
+            # product_info: BeautifulSoup = soup.select('ul.goods_list div.info')
+            product_name: BeautifulSoup = soup.select('ul.goods_list div.info a.tit')
+            product_price: BeautifulSoup = soup.select('ul.goods_list div.info span.price em')
+            product_advertise: BeautifulSoup = soup.select('ul.goods_list div.info span.price a.ad_stk')
+            seller_info: BeautifulSoup = soup.select('ul.goods_list div.info_mall a.mall_img')
+
+            for j, seller in enumerate(seller_info):
+                clean_company_name: str = seller_info[j].text
+                clean_product_name: str = product_name[j].text.strip()
+                clean_product_price: str = product_price[j].text.strip()
+
+                try:
+                    clean_product_advertise: str = f'({product_advertise[i].text.strip()})'
+                except:
+                    clean_product_advertise: str = ''
+
+                if self.company_name in clean_company_name:
+                    result_title = f'{clean_product_name}{clean_product_advertise}'
+                    print(f'{i+1}페이지 {j+1}위 {result_title} {clean_product_price}')
